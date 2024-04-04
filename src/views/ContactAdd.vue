9f121018196c874a2f285bd10d151251c48b14cb
Nguyen Minh Tan <99700061+tantahi28@users.noreply.github.com>
2024-04-04 10:30:38 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm @submit:contact="createContact" />
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được tạo thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
